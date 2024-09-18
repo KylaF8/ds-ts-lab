00c68c964647884653bdb1e7311e001f7e921e37
KylaF8 <20098155@mail.wit.ie>
2024-09-18 11:44:27 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,46 @@
+import { colleagues, friends } from './01-basics'
 import {Friend, Colleague } from './myTypes'
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(f: Friend[]) : string[] {
+    let allOlderArray = [];
+    for (let i = 0; i < f.length; i++) {
+       allOlderArray.push(older(f[i]))
+    }
+    return allOlderArray;
+}
+
+console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+
+  console.log(highestExtension(colleagues.current));
+
+
+  function addColleague(cs: Colleague[], name: string, department: string, email: string): void {
+    const highest = highestExtension(cs).contact.extension
+    const newColleague: Colleague = {
+        name,
+        department,
+        contact: {
+          email,
+          extension: highest + 1 
+        }
+      };
+      
+      cs.push(newColleague);
+    }
+  
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
