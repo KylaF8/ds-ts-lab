22fcfe041db935920ecff530bc646930f2835b75
KylaF8 <20098155@mail.wit.ie>
2024-09-20 11:47:38 +0100

    Define types using Utility types.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -14,8 +14,15 @@ const friend1 : Friend = {
     age: 31,
     interests: ['Music', 'Sport']
   };
-  
-  export const friends = [friend1, friend2];
+
+  const friend3 : Friend = {
+    name: "Patti Burke",
+    phone: "086--12345",
+    age: 31,
+    interests: ['Music', 'Sport']
+  };
+
+  export const friends = [friend1, friend2, friend3];
   //console.log(friends[1]);
   
   //   -------------------
